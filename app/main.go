3c8a4b122a0c4ff7831473ca12c1ac6dab2c1cb8
yosuke.fujii <yosuke.fujii@dena.com>
2015-09-02 20:43:08 +0900

    add main

--- app/main.go
+++ app/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "log"
+import (
+	"log"
+	"github.com/zenazn/goji"
+	"github.com/jinzhu/gorm"
+	_ "github.com/go-sql-driver/mysql"
+)
 
-
+var db gorm.DB
 
 func main() {
 	log.Print("main")
+	goji.Serve()
+}
+
+func init() {
+	db, _ = gorm.Open("mysql", "root:@/searcher?charset=utf8&parseTime=True")
 }
