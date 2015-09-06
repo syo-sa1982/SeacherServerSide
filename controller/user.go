746eddba7f00c775af336a616023718f0e736b4e
syo-sa1982 <fujii1308@gmail.com>
2015-09-07 08:24:04 +0900

    ユーザーチェック用の関数 とりあえず、jsonで結果を返したい

--- controller/user.go
+++ controller/user.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"html/template"
 	"strconv"
+	"encoding/json"
 )
 
 var tpl *template.Template
@@ -25,13 +26,29 @@ func (cntr Controller) UserIndex(c web.C, w http.ResponseWriter, r *http.Request
 	tpl.Execute(w, Users)
 }
 
-
 func (cntr Controller) UserAdd(c web.C, w http.ResponseWriter, r *http.Request){
 	var db = cntr.db
 	r.ParseForm()
 	log.Print("add")
+
+	log.Print(w.Header())
 	log.Print(r.Form)
 	User := model.User{UUID: r.FormValue("uuid")}
 	User.RollCount, _ = strconv.Atoi(r.FormValue("roll_count"))
 	db.Create(&User)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(User)
+}
+
+func (cntr Controller) UserAuth(c web.C, w http.ResponseWriter, r *http.Request){
+	var db = cntr.db
+	log.Print("Auth")
+	User := model.User{}
+	r.ParseForm()
+	User.UUID = r.FormValue("uuid")
+	db.Find(&User)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(User)
 }
--- model/user.go
+++ model/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID int64
 	UUID string `sql:"not NULL;size:36;unique"`
-	Name string `sql:"not NULL;size:255;"`
+	Name string `sql:"size:255;"`
 	RollCount int `sql:"DEFAULT:0"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
