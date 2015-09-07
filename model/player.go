238330abbcf9ab49599691e12274e7a2a0cbdff0
syo-sa1982 <fujii1308@gmail.com>
2015-09-08 08:32:22 +0900

    プレイヤーキャラのモデル * 技能値の管理とか基本能力値以外はどう扱うかとか決めないといけない * サイコロの処理とかは昔C++で作ったクラスを参考に   https://github.com/syo-sa1982/Dice

--- model/player.go
+++ model/player.go
@@ -4,7 +4,15 @@ type Player struct {
 	ID int64
 	UserID int64
 	Name string `sql:"size:255"`
-	HP int
-	San int
+	HP           int
+	Sanity       int
 
+	Strength     int
+	Constitution int
+	Power        int
+	Dextality    int
+	Appeal       int
+	Size         int
+	Intelligence int
+	Education    int
 }
