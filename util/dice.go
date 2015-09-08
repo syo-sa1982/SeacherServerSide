b41078d0e557b6694903c8fda05c998fec77bfeb
syo-sa1982 <fujii1308@gmail.com>
2015-09-09 08:22:20 +0900

    とりあえずダイスを実装はした。 todo: 動作テスト出来てないから後でやる

--- util/dice.go
+++ util/dice.go
@@ -1,2 +1,23 @@
 package util
+import (
+	"math/rand"
+	"time"
+)
 
+type Dice struct {
+	TotalRolls int // 振った回数
+	TotalScore int // 出目の合計
+	RollHistory []int // 出目の履歴
+	
+}
+
+func Roll(surface int, rollcount int) Dice {
+	var dice = Dice{}
+	dice.TotalRolls = rollcount
+	for i := 0; i < rollcount; i++ {
+		rand.Seed(time.Now().UnixNano())
+		dice.RollHistory = append(dice.RollHistory, rand.Intn(surface))
+		dice.TotalScore += dice.RollHistory[i]
+	}
+	return dice
+}
