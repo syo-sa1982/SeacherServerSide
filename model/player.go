db1449b29cd5e7342f3a2d4cbe6637c71b5b114f
syo-sa1982 <fujii1308@gmail.com>
2015-09-17 08:08:14 +0900

    migrateにプレイヤーを追加

--- model/player.go
+++ model/player.go
@@ -1,12 +1,11 @@
 package model
 
 type Player struct {
-	ID int64
+	PlayerID int64
 	UserID int64
 	Name string `sql:"size:255"`
 	HP           int
 	Sanity       int
-
 	Strength     int
 	Constitution int
 	Power        int
