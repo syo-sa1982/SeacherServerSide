e3d243d887d6d7a542bf6f54d20cdb04d4cb02e7
syo-sa1982 <fujii1308@gmail.com>
2015-11-06 00:16:01 +0900

    PlayerSkillの定義が間違ってた

--- db/migrate.go
+++ db/migrate.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
 	"os"
+	"log"
 )
 
 
@@ -26,28 +27,36 @@ func main(){
 		panic(err)
 	}
 
+	log.Println("User")
 	if db.HasTable(&model.User{}) { db.DropTable(&model.User{}) }
 	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&model.User{})
 
+	log.Println("PlayerBase")
 	if db.HasTable(&model.PlayerBase{}) { db.DropTable(&model.PlayerBase{}) }
 	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&model.PlayerBase{})
 
+	log.Println("PlayerStatus")
 	if db.HasTable(&model.PlayerStatus{}) { db.DropTable(&model.PlayerStatus{}) }
 	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&model.PlayerStatus{})
 
+	log.Println("PlayerSkill")
 	if db.HasTable(&model.PlayerSkill{}) { db.DropTable(&model.PlayerSkill{}) }
 	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&model.PlayerSkill{})
 
+	log.Println("SkillCategory")
 	if db.HasTable(&model.SkillCategory{}) { db.DropTable(&model.SkillCategory{}) }
 	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&model.SkillCategory{})
 
+	log.Println("SkillCategory INSERT")
 	for key := range model.SkillCategoryList{
 		db.Create(&model.SkillCategoryList[key])
 	}
 
+	log.Println("SkillMaster")
 	if db.HasTable(&model.SkillMaster{}) { db.DropTable(&model.SkillMaster{}) }
 	db.Set("gorm:table_options", "ENGINE=InnoDB").CreateTable(&model.SkillMaster{})
 
+	log.Println("SkillMaster INSERT")
 	for key := range model.SkillMasterList{
 		db.Create(&model.SkillMasterList[key])
 	}
