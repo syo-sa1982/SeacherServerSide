793b1939e6fd93594cc94f5b992eb4588a4cd8f6
syo-sa1982 <fujii1308@gmail.com>
2015-12-20 18:00:12 +0900

    ユーザー情報を表示させとく

--- controller/main_menu.go
+++ controller/main_menu.go
@@ -5,15 +5,37 @@ import (
 	"github.com/syo-sa1982/SeacherServerSide/model"
 	"log"
 	"html/template"
+	"encoding/json"
 )
 
 
 
 func (cntr Controller) MainIndex(c web.C, w http.ResponseWriter, r *http.Request) {
 	var db = cntr.db
-	Users := []model.User{}
-	db.Find(&Users)
-	log.Print("index")
-	tpl = template.Must(template.ParseFiles("view/user/index.html"))
-	tpl.Execute(w, Users)
+	var UserInfo UserInfoAPI
+	var (
+		User = model.User{}
+		PlayerBase = model.PlayerBase{}
+		Player = model.PlayerStatus{}
+		Job = model.JobMaster{}
+	)
+
+	r.ParseForm()
+	log.Println(r.FormValue("UUID"))
+
+	db.Model(User).Where("uuid = ?", r.FormValue("UUID")).Find(&User)
+	db.Model(PlayerBase).Where("user_id = ?", User.ID).Find(&PlayerBase)
+	db.Model(Player).Where("user_id = ?", User.ID).Find(&Player)
+
+	log.Println(Player.JobID)
+	db.Model(Job).Where("id = ?", Player.JobID).Find(&Job)
+
+	UserInfo.User = User
+	UserInfo.Job = Job
+	UserInfo.PlayerBase = PlayerBase
+	UserInfo.PlayerStatus = Player
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(UserInfo)
+
 }
