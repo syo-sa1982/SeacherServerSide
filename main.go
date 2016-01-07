de81364e6c67cd5c74bd4978c2f36babc2bc4f63
syo-sa1982 <fujii1308@gmail.com>
2016-01-08 07:13:16 +0900

    ルーティングを修正

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func rooter(m *web.Mux) http.Handler {
 	m.Post("/player/joblist", cntr.JobList)
 	m.Post("/player/base_make", cntr.PlayerBaseMake)
 	m.Post("/player/generate", cntr.PlayerGenerate)
-	m.Get("/player/skill_setting", cntr.SkillSetting)
+	m.Post("/player/skill_setting", cntr.SkillSetting)
 	m.Post("/player/skill_submit", cntr.SkillSubmit)
 
 	m.Post("/home/user/info", cntr.UserInfo)
